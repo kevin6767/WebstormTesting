f4c59f01b62b077e4d8e8ac1eb6252c901c55fba
Kevin Eslick <eslick.kevin@yahoo.com>
2020-01-13 19:08:09 -0600

    using await and axios to change things up but not receiving anything
    Signed-off-by: Kevin Eslick <eslick.kevin@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,73 +1,18 @@
-import React, { Component } from 'react'
+import React, {Component, useEffect, useState} from 'react'
+import axios from 'axios'
+require("regenerator-runtime/runtime");
 
-const Heroes = ({heroes , id}) => {
-    return (
-        <div>
-            <h1>Hero List</h1>
-            <select name={heroes} key={id} onChange={this.handleChange} value={this.state.value}>
-                {heroes.map(hero =>
-                    <option>{hero.localized_name}
-                    </option>
-                )};
-            </select>
-        </div>
-    )
-};
-const Hero = ({selectedHero}) => {
+const App = () => {
+    const [data, setData] = useState({heroes: []});
+    useEffect(() =>
+        async function f() {
+            const result = await axios(
+                    'https://api.opendota.com/api/heroStats',
+                );
+                    setData(result.data);
+            },[]);
     return(
-        <div>
-            <h1>
-                Does this work? {selectedHero}
-            </h1>
-        </div>
+        <h2>{data.heroes}</h2>
     )
 };
-
-class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            heroes: [],
-            selectedHero: null,
-            value: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
-
-    }
-    componentDidMount() {
-        let initialHeroes = [];
-        fetch('https://api.opendota.com/api/heroStats')
-            .then(res => res.json())
-            .then((data) => {
-                initialHeroes = data.map((heroes) => {
-                    return heroes
-                });
-                console.log(initialHeroes);
-                this.setState({
-                    heroes: initialHeroes,
-                })
-            })
-            .catch(console.log)
-    }
-
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    render() {
-        const {selectedHero} = this.state;
-        return (
-            <>
-                <h1>Here Comes Angular {selectedHero}</h1>
-                <Heroes selectedHero={selectedHero} heroes={this.state.heroes} handleChange={this.handleChange} />
-                {selectedHero && <Hero selectedHero={selectedHero}/>}
-                <Hero />
-            </>
-        )
-    }
-}
-
-
 export default App
