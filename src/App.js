67928afc7f0288abe7b9779a95cfddf60010a20e
Kevin Eslick <eslick.kevin@yahoo.com>
2020-01-13 23:25:14 -0600

    rewrote the code to functional and added onChange events! Finally working!
    Signed-off-by: Kevin Eslick <eslick.kevin@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, {Component, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 require("regenerator-runtime/runtime");
 
 const App = () => {
-    const [data, setData] = useState({heroes: []});
-    useEffect(() =>
-        async function f() {
-            const result = await axios(
-                    'https://api.opendota.com/api/heroStats',
-                );
-                    setData(result.data);
-            },[]);
+    const [hero, selectedHero] = useState(
+        'Select a Hero'
+    );
+    const handleChange = event => selectedHero(event.target.value);
     return(
-        <h2>{data.heroes}</h2>
+        <HeroSelect heroSelect={hero} onChangeHeadline={handleChange} />
+    );
+
+};
+const HeroSelect = ({heroSelect, onChangeHeadline}) => {
+    const [data, setData] = useState({heroes: []});
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                'https://api.opendota.com/api/heroStats',
+            );
+            setData({...data, heroes: result.data});
+        };
+        fetchData();
+    }, []);
+    return (
+        <div>
+            <h1>{heroSelect}</h1>
+            <select value={heroSelect} onChange={onChangeHeadline} >
+                {data.heroes.map(item => (
+                    <option>
+                        {item.localized_name}
+                    </option>
+                ))}
+            </select>
+        </div>
     )
 };
 export default App
