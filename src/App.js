585ace7e2c934200b774b62aa4d2eb920afce120
Kevin Eslick <eslick.kevin@yahoo.com>
2020-01-15 19:10:11 -0600

    Now I'm able to produce movement speed from the select id. Gong to work on the next project now!
    Signed-off-by: Kevin Eslick <eslick.kevin@yahoo.com>

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import axios from 'axios'
 require("regenerator-runtime/runtime");
 
 const App = () => {
-    const [hero, selectedHero] = useState(
+    const [hero,selectedHero] = useState(
         'Select a Hero'
     );
-    const handleChange = event => selectedHero(event.target.value);
+    const [heroDets, selectedheroDets] = useState(
+        'Movement Speed'
+    );
+    const handleChange = event => {
+        // event is an object which contains the value and id from the child component.
+        selectedHero(event.value);
+        selectedheroDets(event.id);
+    };
     return(
-        <HeroSelect heroSelect={hero} onChangeHeadline={handleChange} />
+        <HeroSelect heroDetails={heroDets} heroSelect={hero} onChangeHeadline={handleChange} />
     );
 
 };
-const HeroSelect = ({heroSelect, onChangeHeadline}) => {
+const HeroSelect = ({heroSelect, heroDetails, onChangeHeadline}) => {
     const [data, setData] = useState({heroes: []});
     useEffect(() => {
         const fetchData = async () => {
@@ -23,17 +30,23 @@ const HeroSelect = ({heroSelect, onChangeHeadline}) => {
         };
         fetchData();
     }, []);
+    const getSelected=event=>{
+        // added an extra function here, which passes the value and id to the app component.
+        onChangeHeadline({value: event.target.value, id:data.heroes[event.target.selectedIndex].move_speed})
+    };
     return (
         <div>
-            <h1>{heroSelect}</h1>
-            <select value={heroSelect} onChange={onChangeHeadline} >
+            <select id={heroDetails} value={heroSelect} onChange={getSelected} >
                 {data.heroes.map(item => (
-                    <option>
+                    <option id={item.move_speed}>
                         {item.localized_name}
                     </option>
                 ))}
             </select>
+            <h1>{heroSelect}</h1>
+            <h1>{heroDetails}</h1>
         </div>
+
     )
 };
 export default App
